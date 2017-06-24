c1e76fa2a715c4fda2fed8a07bde7958a6a3daa1
whalemare <nanostet@gmail.com>
2017-06-24 11:26:03 +0700

    Change min api to 16

--- kotlin-script.kts
+++ kotlin-script.kts
@@ -1,1 +1,29 @@
-println("HEEELLLOOOOO WOOOORRRLLDD!!!!")
+#!/usr/bin/env kscript
+//DEPS com.offbytwo.jenkins:jenkins-client:0.3.7
+
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
+class App {
+    val jenkinsUrl = URL("http://localhost:8080/job/itemTest/build?token=whalemareToken")
+
+    fun main() {
+
+        jenkinsUrl.openConnection()
+        val response = (jenkinsUrl.openConnection() as HttpURLConnection).apply {
+            requestMethod = "GET"
+            addRequestProperty(
+                    "User-Agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
+            )
+        }.inputStream
+
+        val scanner = Scanner(response)
+        println(scanner.useDelimiter("\\A").next())
+    }
+}
+
+App().main()
+
+println("Script success-full")
